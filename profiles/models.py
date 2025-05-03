c0825840f166be65f22eb21c126dd9ef8a45a108
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-05-03 21:32:24 +0200

    Add profile serializers and list view

--- profiles/models.py
+++ profiles/models.py
@@ -7,7 +7,8 @@ class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=250, blank=False)
+    name = models.CharField(max_length=250, blank=True)
+    content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_profile_sa43oj'
     )
