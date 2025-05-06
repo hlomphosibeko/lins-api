c0e4f1412eac3fea00f1904ebea2bcfe496bd211
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-05-06 15:00:19 +0200

    Add JSON Web Token authentication to my project

--- env.py
+++ env.py
@@ -1,3 +1,3 @@
 import os
-os.environ[
-    'CLOUDINARY_URL'] = 'cloudinary://683322417993834:FqCJYTpinRPWU_oEgba-HmcTYKE@dibjhrwcc'
+os.environ['CLOUDINARY_URL'] = 'cloudinary://683322417993834:FqCJYTpinRPWU_oEgba-HmcTYKE@dibjhrwcc'
+os.environ['DEV'] = '1'
