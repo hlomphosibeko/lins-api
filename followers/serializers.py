2ec5af8a01349b0f6e0c8655994782b1240338fa
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-05-10 14:34:49 +0200

    Create Follower models, serializers and generic views

---
+++ followers/serializers.py
@@ -0,0 +1,20 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from followers.models import Follower
+
+
+class FollowerSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    followed_name = serializers.ReadOnlyField(source='followed.username')
+
+    class Meta:
+        model = Follower
+        fields = ['id', 'owner', 'followed', 'created_at', 'followed_name']
+
+    def create(self, validate_data):
+        try:
+            return super().create(validate_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
--- lins_api/settings.py
+++ lins_api/settings.py
@@ -88,6 +88,7 @@ INSTALLED_APPS = [
     'dj_rest_auth.registration',
     'corsheaders',
     'likes',
+    'followers',
     'comments',
     'posts',
     'profiles',
