33c74b03d43260d56ab63d681844f25ffbb57911
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-05-13 20:29:10 +0200

    Create signup page, Add content on the README.md file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
 import {Route, Switch} from 'react-router-dom';
+import RegisterForm from './pages/auth/RegisterForm';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home Page</h1>} />
           <Route exact path="/login" render={() => <h1>Log In</h1>} />
-          <Route exact path="/register" render={() => <h1>Register</h1>} />
+          <Route exact path="/register" render={() => <RegisterForm />} />
           <Route render={() => <p>Page not found!</p>} /> 
         </Switch>
         
