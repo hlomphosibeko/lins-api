a86e08a6582010f5092a7a830e98d7f815eecbbc
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-05-14 22:27:20 +0200

    Update Signin and signup forms with the correct content, add axios data

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,48 +1,48 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import logo from '../assets/lobolo-logo.png';
-import styles from '../styles/NavBar.module.css';
-import {NavLink} from 'react-router-dom';
-
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/lobolo-logo.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <Navbar className={styles.NavBar} expand="mb" fixed='top'>
-            <Container>
-                <NavLink to="/">
-                    <Navbar.Brand>
-                        <img src={logo} alt="logo" />
-                    </Navbar.Brand>
-                </NavLink>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto text-right">
-                        <NavLink
-                         exact
-                         to="/"
-                         className={styles.NavLink}
-                         activeClassName={styles.Active}>
-                            Home
-                        </NavLink>
-                        <NavLink
-                         to="/login"
-                         className={styles.NavLink}
-                         activeClassName={styles.Active}>
-                            Log In
-                        </NavLink>
-                        <NavLink
-                         to="/register"
-                         className={styles.NavLink}
-                         activeClassName={styles.Active}>
-                            Register
-                        </NavLink>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
-}
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i className="fas fa-user-plus"></i>Sign up
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavBar;
