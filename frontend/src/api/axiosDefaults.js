2b3d804add2b20cab731f09fcfca157601e14efb
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-07-11 09:49:13 +0200

    Comment out baseURL value while developing project

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -7,8 +7,8 @@
 
 import axios from "axios";
 
-axios.defaults.baseURL = "/api";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+// axios.defaults.baseURL = "/api";
+axios.defaults.headers.post["content-type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
