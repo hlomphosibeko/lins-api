5bfce87eb244b31365972a5b3d1797a9d40b414a
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-07-29 16:31:27 +0200

    Preparing workspace for deployement, Change buttons to match site

--- frontend/src/pages/profiles/Profile.js
+++ frontend/src/pages/profiles/Profile.js
@@ -41,7 +41,7 @@ const Profile = (props) => {
             </Button>
           ) : (
             <Button
-              className={`${btnStyles.Button} ${btnStyles.Black}`}
+              className={`${btnStyles.Button} ${btnStyles.Brown}`}
               onClick={() => handleFollow(profile)}
             >
               follow
